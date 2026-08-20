f8931900775d999493f55af46f80e62dd5d2a29a
Sofia <sofiiman2025@gmail.com>
2026-08-20 12:38:05 -0300

    .

--- pryED_Manassero/clsCola.cs
+++ pryED_Manassero/clsCola.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace pryED_Manassero
 {
@@ -13,5 +14,34 @@ namespace pryED_Manassero
 
         internal clsNodo Primero1 { get => Primero; set => Primero = value; }
         internal clsNodo Ultimo1 { get => Ultimo; set => Ultimo = value; }
+
+
+        public void Agregar(clsNodo Nuevo)
+        {
+            if (Primero == null)
+            {
+                Primero = Nuevo;
+
+                Ultimo = Nuevo;
+            }
+            else
+            {
+                Ultimo.Siguiente = Nuevo;
+                Ultimo = Nuevo;
+            }
+
+        }
+
+        public void Recorrer(ListBox lstLista) 
+        {
+            clsNodo aux = Primero;
+            lstLista.Items.Clear();
+            while (aux != null)
+            {
+                lstLista.Items.Add(aux.Codigo1);
+                aux = aux.Siguiente;
+            }
+
+        }
     }
 }
